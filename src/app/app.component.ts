2c76f8e638e162e86e132eeb2fc6d4328a15fcb5
Mantas Antanaitis <antanaitis.web@gmail.com>
2022-09-26 15:20:27 +0300

    success action screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  additionalResources = [{
+    description: "Learn more about LIMBO while you're waiting, read our customer stories and see how the program transformed theirs lives, or go see our blog article to learn about amazing LIMBO science.",
+    links: [{
+      title: "customer stories",
+      url: "#"
+    }, {
+      title: "limbo blog",
+      url: "#"
+    }]
+  }, {
+    description: "You can prepare for your LIMBO journey! Watch our tutorials, so when you're selected, you can jump right in and get started with the program",
+    links: [{
+      title: "how to use app",
+      url: "#"
+    }, {
+      title: "how to setup devices",
+      url: "#"
+    }]
+  }]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: "#0085f4",
+        body: "#000000",
+        "body-contrast": "#151515"
+      },
+      textColor: {
+        primary: "#ffffff",
+        secondary: "#ababab",
+        link: "#0085f4"
+      }
+    },
   },
   plugins: [],
 }
